d7a717f379bbae1992a348c7b8690238bf8abe26
kim <1347408207@qq.com>
2021-02-05 18:37:01 +0800

    脚手架完成

--- bin/cli-init.js
+++ bin/cli-init.js
@@ -3,15 +3,18 @@
 const program = require('commander');
 const Project = require('../lib/project');
 
+// 自定义标志 options
 program
-  .option('--name [name]', '项目名称')
+  .option('--name [name]', 'project name')
   .parse(process.argv)
 
 const { name } = program
 const args = program.args
 
+// 项目名称
 const projectName = args[0] || name
 
+// 初始化项目
 const project = new Project({
   projectName
 })
--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 const program = require('commander') // 命令行工具
+const {logPackageVersion} = require('../lib/utils')
 
-// command 添加命令
+logPackageVersion()
+// 命令解析
 program.version('1.0.0')
   .usage('<command> [options]')
   .command('init [name]', 'init a project')
